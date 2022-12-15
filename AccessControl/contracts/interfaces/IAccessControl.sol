ca90893328e2e71f3d3d9bc62b2bb2d1628acabe
Top-Kraken <topkyle521@gmail.com>
2022-12-15 09:59:29 -0500

    update IAccessControl

--- AccessControl/contracts/interfaces/IAccessControl.sol
+++ AccessControl/contracts/interfaces/IAccessControl.sol
@@ -48,25 +48,86 @@ interface IAccessControl {
      */
     event RoleRenounced(bytes32 indexed role, address indexed account);
 
+    /**
+     * @dev Returns `true` if `account` has been granted `role`.
+     */
     function hasRole(bytes32 role, address account)
         external
         view
         returns (bool);
 
+    /**
+     * @dev Grants `role` to `account`.
+     *
+     * If `account` had not been already granted `role`, emits a {RoleGranted}
+     * event.
+     *
+     * Requirements:
+     *
+     * - the caller must have ``role``'s admin role.
+     */
     function grantRole(bytes32 role, address account) external;
 
+    /**
+     * @dev Revokes `role` from `account`.
+     *
+     * If `account` had been granted `role`, emits a {RoleRevoked} event.
+     *
+     * Requirements:
+     *
+     * - the caller must have ``role``'s admin role.
+     */
     function revokeRole(bytes32 role, address account) external;
 
+    /**
+     * @dev Revokes `role` from the calling account.
+     *
+     * Roles are often managed via {grantRole} and {revokeRole}: this function's
+     * purpose is to provide a mechanism for accounts to lose their privileges
+     * if they are compromised (such as when a trusted device is misplaced).
+     *
+     * If the calling account had been granted `role`, emits a {RoleRenounced}
+     * event.
+     *
+     * Requirements:
+     *
+     * - the caller must be `account`.
+     */
     function renounceRole(bytes32 role, address account) external;
 
+    /**
+     * @dev Sets `adminRole` as ``role``'s admin role.
+     *
+     * Emits a {RoleAdminChanged} event.
+     */
     function setRoleAdmin(bytes32 role, bytes32 adminRole) external;
 
+    /**
+     * @dev Returns the admin role that controls `role`. See {grantRole} and
+     * {revokeRole}.
+     *
+     * To change a role's admin, use {AccessControl-_setRoleAdmin}.
+     */
     function getRoleAdmin(bytes32 role) external view returns (bytes32);
 
+    /**
+     * @dev Returns one of the accounts that have `role`. `index` must be a
+     * value between 0 and {getRoleCount}, non-inclusive.
+     *
+     * Role bearers are not sorted in any particular way, and their ordering may
+     * change at any point.
+     *
+     * WARNING: When using {getRoleAt} and {getRoleCount}, make sure
+     * you perform all queries on the same block.
+     */
     function getRoleAt(bytes32 role, uint256 index)
         external
         view
         returns (address);
 
+    /**
+     * @dev Returns the number of accounts that have `role`. Can be used
+     * together with {getRoleAt} to enumerate all bearers of a role.
+     */
     function getRoleCount(bytes32 role) external view returns (uint256);
 }
