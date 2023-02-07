19420a048350b4fc53e67949d380d5aaabeb5afc
Top-Kraken <topkyle521@gmail.com>
2023-02-07 07:10:35 -0500

    Add uniswap v3 core interfaces and libraries

--- DexPriceAggregatorUniswapV3/contracts/DexPriceAggregatorUniswapV3.sol
+++ DexPriceAggregatorUniswapV3/contracts/DexPriceAggregatorUniswapV3.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
+import "./interfaces/IUniswapV3Pool.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/SafeCast.sol";
 
--- DexPriceAggregatorUniswapV3/contracts/libraries/OracleLibrary.sol
+++ DexPriceAggregatorUniswapV3/contracts/libraries/OracleLibrary.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.0;
 
-import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
-import "@uniswap/v3-core/contracts/libraries/LowGasSafeMath.sol";
+import "../interfaces/IUniswapV3Pool.sol";
+import "./LowGasSafeMath.sol";
 import "./FullMath.sol";
 import "./TickMath.sol";
 import "./PoolAddress.sol";
