9467d26e6cbfbd3beb4e43a5bb9e33c3437e2adc
deep-quality-dev <hungrywarrior081@gmail.com>
2023-03-15 09:02:54 -0400

    Remove unused files & gas optimization

--- StakingERC721/contracts/mock/MockERC721.sol
+++ StakingERC721/contracts/mock/MockERC721.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.9;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {Counters} from "@openzeppelin/contracts/utils/Counters.sol";
